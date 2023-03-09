6e3256dfb3f077d58b78118b2c0794744b26bc49
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-03-09 21:21:36 +0100

    ciclo for

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,18 @@
-    let i=1;
-    while(i<35)
-        {
-let Element= document.getElementById("profile");
-let clone = Element.cloneNode(true);
-clone.id="coppia"+1;
-Element.after(clone);
-i=i+1
+//     let i=1;
+//     while(i<35)
+//         {
+// let Element= document.getElementById("profile");
+// let clone = Element.cloneNode(true);
+// clone.id="coppia";
+// Element.after(clone);
+// i=i+1
+// }
+
+let i=1;
+
+for(let i=1; i<35; i++){
+    let Element = document.getElementById("profile")
+    let clone = Element.cloneNode(true);
+    clone.id= "oggetto clonato";
+    Element.after(clone);
 }
