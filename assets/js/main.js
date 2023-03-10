126153f8b204f7c731c9421f1b35ec145a4df3e3
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-03-10 09:33:50 +0100

    fix main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,16 @@
-//     let i=1;
-//     while(i<35)
-//         {
-// let Element= document.getElementById("profile");
-// let clone = Element.cloneNode(true);
-// clone.id="coppia";
-// Element.after(clone);
-// i=i+1
+
+// for(let i=1; i<35; i++){
+//     let Element = document.getElementById("profile")
+//     let clone = Element.cloneNode(true);
+//     clone.id= "oggetto clonato";
+//     Element.after(clone);
 // }
 
 let i=1;
-
-for(let i=1; i<35; i++){
-    let Element = document.getElementById("profile")
+while(i<35){
+    let Element=document.getElementById("profile");
     let clone = Element.cloneNode(true);
-    clone.id= "oggetto clonato";
+    clone.id="clone";
     Element.after(clone);
+    i=i+1;
 }
